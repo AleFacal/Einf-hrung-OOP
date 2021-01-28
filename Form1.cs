a341333dd7581b144f3730652127572e3d6798d4
AleFacal <75016125+AleFacal@users.noreply.github.com>
2021-01-28 11:17:54 +0100

    Add files via upload
    Einen kleinen Fehler beim Design, aber sonst funktioniert alles.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Aufgabe_07_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WordCount_Click(object sender, EventArgs e)
+        {
+            WordTab wrdTab = new WordTab();
+            Parser parser = new Parser(wrdTab);
+            try
+            {
+                parser.Parse(textBox1.Text);
+                String table = wrdTab.GetTab();
+                wrdTab.PrintTab(table, OutputBox);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
